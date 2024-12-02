6708ff14d183041f3c8e884d9cd489af276bedb0
Anamul Haque <anamulhaque9901@gmail.com>
2024-12-02 13:49:46 +0600

    Create user

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,11 +1,11 @@
-import { HttpStatus } from "http-status-ts";
+import { Request } from "express";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import { UserService } from "./user.service";
 import { StatusCodes } from "http-status-codes";
 
 const createUser = catchAsync(async (req, res) => {
-  const result = await UserService.createUser();
+  const result = await UserService.createUser(req.body);
 
   sendResponse(res, {
     statusCode: StatusCodes.OK,
--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,5 +1,11 @@
-const createUser = async () => {
-  console.log("create user");
+import prisma from "../../../shared/prisma";
+
+const createUser = async (payload: any) => {
+  console.log("create user", payload);
+  const result = await prisma.user.create({
+    data: payload
+  })
+  return result
 };
 
 export const UserService = {

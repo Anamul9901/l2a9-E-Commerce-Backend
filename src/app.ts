16fae0315c4b71a93c58f22c29f58c6aa7648d9c
Anamul Haque <anamulhaque9901@gmail.com>
2024-12-02 13:52:57 +0600

    Error handaling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 
@@ -17,7 +18,19 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 
-app.use('/api/v1', router)
+app.use("/api/v1", router);
 
+app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "API not found!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
+});
 
 export default app;
